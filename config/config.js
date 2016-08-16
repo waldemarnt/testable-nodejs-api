ca7cc8b335d7aca923d27e7b8a40c6f1af44dad3
Waldemar Neto <waldemarnt@gmail.com>
2016-08-15 23:22:08 -0300

    Fixes configuration

--- config/config.js
+++ config/config.js
@@ -1,12 +1,13 @@
 export default {
-  database: 'library',
+  database: 'books',
   username: '',
   password: '',
   params: {
     dialect: 'sqlite',
-    storage: process.env.TEST ? 'test_library.sqlite' : 'library.sqlite',
+    storage: `${process.env.NODE_ENV}_books.sqlite`,
     define: {
       underscored: true,
     },
   },
 };
+
